af6d6904da0893a6848fb5a2a36014989ba07877
Jeishanul Haque Shishir <razinsoft.shishir@gmail.com>
2023-10-30 16:22:56 +0600

    first comment

---
+++ resources/views/bkash-payment.blade.php
@@ -0,0 +1,11 @@
+<script src="https://code.jquery.com/jquery-3.4.1.min.js"
+    integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo=" crossorigin="anonymous"></script>
+
+<script id="myScript" src="https://scripts.sandbox.bka.sh/versions/1.2.0-beta/checkout/bKash-checkout-sandbox.js">
+</script>
+
+<button class="btn btn-success" id="bKash_button" onclick="BkashPayment()">
+    Pay with bKash
+</button>
+
+@include('bkash-script')
---
+++ routes/web.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\BkashPaymentController;
+use App\Http\Controllers\BkashRefundController;
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+// Payment Routes for bKash
+Route::controller(BkashPaymentController::class)
+    ->group(function () {
+        Route::get('/', 'index');
+        Route::get('/get-token', 'getToken')->name('bkash-get-token');
+        Route::get('/create-payment', 'createPayment')->name('bkash-create-payment');
+        Route::post('/execute-payment', 'executePayment')->name('bkash-execute-payment');
+        Route::get('/query-payment', 'queryPayment')->name('bkash-query-payment');
+        Route::post('/success', 'bkashSuccess')->name('bkash-success');
+    });
+
+// Refund Routes for bKash
+Route::controller(BkashRefundController::class)->group(function () {
+    Route::post('/refund', 'refund');
+});
